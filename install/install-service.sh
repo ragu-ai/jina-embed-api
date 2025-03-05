50bab7de4c99a4da55f36e8cc9356b2190b7d4f7
kevywilly <kevin.williams@sidusa.com>
2025-03-05 14:46:51 -0500

    update service name

--- install/install-service.sh
+++ install/install-service.sh
@@ -1,4 +1,4 @@
-sudo cp /home/ec2-user/jina-embed-api/install/embed-api.service /etc/systemd/system/
+sudo cp /home/ec2-user/jina-embed-api/install/jina-embed-api.service /etc/systemd/system/
 sudo systemctl daemon-reload
 sudo systemctl enable embed-api
 sudo systemctl start embed-api
